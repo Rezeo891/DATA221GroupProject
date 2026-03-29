acd3b7e3c930b513166e30263384fc12f0bd7423
waliarudraksh <waliarudraksh@gmail.com>
2026-03-29 15:27:10 -0600

    add imports for svm and evaluation

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.svm import SVC
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_curve,
+    roc_auc_score,
+    confusion_matrix
+)
