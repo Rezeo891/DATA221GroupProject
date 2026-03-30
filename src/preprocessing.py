6b8ce368dc3fca8da9fb28d62e6d57597cf08715
Rezeo891 <lucasz89261@gmail.com>
2026-03-29 21:50:28 -0600

    Separate features and target

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,3 +6,7 @@ from sklearn.preprocessing import StandardScaler
 def load_and_preprocess():
     # Load dataset from data folder
     df = pd.read_csv("../data/water_potability.csv")
+
+    # Separate features and target
+    X = df.drop("Potability", axis=1)
+    y = df["Potability"]
